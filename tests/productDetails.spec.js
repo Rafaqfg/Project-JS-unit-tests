cd22f0db1a09d5e858ed9928b22be5dc86c52463
Rafael Godoy <rafa.qfg@gmail.com>
2022-01-22 10:28:27 -0300

    requisito 6

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -28,13 +28,23 @@ const productDetails = require('../src/productDetails');
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    fail('Teste vazio!');
+    // fail('Teste vazio!');
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se productDetails é uma função.
+    expect(typeof productDetails).toBe('function');
     // Teste se o retorno da função é um array.
+    expect(Array.isArray(productDetails('pdt1', 'pdt2'))).toBe(true);
     // Teste se o array retornado pela função contém dois itens dentro.
+    expect(productDetails().length).toBe(2);
     // Teste se os dois itens dentro do array retornado pela função são objetos.
+    expect(typeof productDetails()).toBe('object');
     // Teste se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.
+    expect(productDetails('pdt1', 'pdt2')[0]).not.toEqual(productDetails('pdt1', 'pdt2')[1]);
     // Teste se os dois productIds terminam com 123.
+    expect(productDetails('pdt1', 'pdt2')[0].details.productId.endsWith('123'));
+    expect(productDetails('pdt1', 'pdt2')[1].details.productId.endsWith('123'));
   });
 });
+// bibliografia utilizada:
+// https://jestjs.io/pt-BR/docs/expect#toequalvalue
+// https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/String/endsWith
